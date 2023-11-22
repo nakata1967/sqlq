1a234bc17833be93c496f4250dc46aceef089b56
nakata1967 <naka.sql.note@gmail.com>
2023-11-22 16:51:00 +0900

    ver.1

---
+++ q04.sql
@@ -0,0 +1,12 @@
+SELECT
+  COUNT(DISTINCT user_session) AS total_sessions
+FROM (
+  SELECT
+    /* CAST(event_params.value.int_value AS STRING): ここではCAST関数を使って、整数値（int_value）を文字列（STRING）に型変換しています。SQLでは、異なるデータ型を結合する前に同じ型に揃える必要があります。 */ CONCAT(user_pseudo_id, CAST(event_params.value.int_value AS STRING)) AS user_session
+  FROM
+    `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+    UNNEST(event_params) AS event_params
+  WHERE
+    _TABLE_SUFFIX BETWEEN '20201101'
+    AND '20201110'
+    AND event_params.key = 'ga_session_id' );
