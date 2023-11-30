471c74fcf4ce897020cae9579cc1583d836c6e94
nakata1967 <naka.sql.note@gmail.com>
2023-11-30 19:17:43 +0900

    ver.1

--- discord01.sql
+++ discord01.sql
@@ -5,8 +5,7 @@ SELECT
     (event_params.key = 'page_title', event_params.value.string_value, NULL)) AS page_title,
   MAX(
   IF
-    (event_params.key = 'page_location'
-      AND event_name LIKE '%google%', event_params.value.string_value, NULL)) AS page_location,
+    (event_params.key = 'page_location', event_params.value.string_value, NULL)) AS page_location,
   COUNT(event_name) AS pageviews,
   COUNT(DISTINCT user_pseudo_id) AS users
 FROM
@@ -17,6 +16,8 @@ WHERE
   AND '20201110'
 GROUP BY
   date
+HAVING
+  page_location LIKE '%google%'
 ORDER BY
   date,
   pageviews DESC,
