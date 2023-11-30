6adaf95791de008ebf3ac735367b53e37ab529f4
nakata1967 <naka.sql.note@gmail.com>
2023-11-30 19:01:30 +0900

    ver.1

---
+++ discord01.sql
@@ -0,0 +1,23 @@
+SELECT
+  DATE(TIMESTAMP_MICROS(event_timestamp), "Asia/Tokyo") AS date,
+  MAX(
+  IF
+    (event_params.key = 'page_title', event_params.value.string_value, NULL)) AS page_title,
+  MAX(
+  IF
+    (event_params.key = 'page_location'
+      AND event_name LIKE '%google%', event_params.value.string_value, NULL)) AS page_location,
+  COUNT(event_name) AS pageviews,
+  COUNT(DISTINCT user_pseudo_id) AS users
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+  UNNEST(event_params) AS event_params
+WHERE
+  _TABLE_SUFFIX BETWEEN '20201101'
+  AND '20201110'
+GROUP BY
+  date
+ORDER BY
+  date,
+  pageviews DESC,
+  users DESC;
